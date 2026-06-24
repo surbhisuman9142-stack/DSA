6d11157235cd0856b96ec323dd3c415bee79fdf6
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-06-24 17:09:14 +0530

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number:"))
+smallest = a
+if b < smallest:
+    smallest = b
+if c < smallest:
+    smallest = c
+print(smallest)
